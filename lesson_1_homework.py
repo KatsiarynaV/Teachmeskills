983307d6b218933abf993d32683b9fc82e77db04
Katsiaryna <68267190+KatsiarynaV@users.noreply.github.com>
2025-02-13 09:46:51 +0300

    Python homework

---
+++ lesson_1_homework.py
@@ -0,0 +1,71 @@
+# 1 сложение чисел
+num_1 = 7
+num_2 = 15
+result_1 = num_1 + num_2
+
+print(result_1)
+
+# 2 вычитание чисел
+num_3 = 25
+num_4 = 10
+result_2 = num_3 - num_4
+
+print(result_2)
+
+# 3 умножение чисел
+num_5 = 6
+num_6 = 8
+result_3 = num_5 * num_6
+
+print(result_3)
+
+# 4 деление чисел
+num_7 = 20
+num_8 = 4
+result_4 = num_7 / num_8
+
+print(result_4)
+
+# 5 комбинация операций
+num_9 = 10
+num_10 = 5
+num_11 = 2
+result_5 = (10 + 5) * 2
+
+print(result_5)
+
+# 6 остаток от деления
+num_12 = 17
+num_13 = 3
+result_6 = num_12 % num_13
+
+print(result_6)
+
+# 7 несколько операций
+num_14 = 15
+num_15 = 3
+num_16 = 2
+num_17 = 4
+num_18 = 6
+result_7 = (num_14 - num_15) * (num_16 + num_17) / num_18
+
+print(result_7)
+
+# 8 дроби
+num_19 = 22
+num_20 = 7
+result_8 = num_19 // num_20
+
+print(result_8)
+
+# 9 сложное выражение
+result_9 = (num_9 + num_7) / num_10 + num_15 * (num_6 - num_16)
+
+print(result_9)
+
+# 10 операции со скобками
+num_21 = 100
+num_22 = 30
+result_10 = num_21 - (num_22 / (num_16 + num_15)) * num_17 + num_9
+
+print(result_10)
