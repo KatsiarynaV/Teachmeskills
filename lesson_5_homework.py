d2c83c55fcc25053d3cab1c5733a2ec00e9387e3
Katsiaryna <68267190+KatsiarynaV@users.noreply.github.com>
2025-02-14 16:02:19 +0300

    Homework 5

---
+++ lesson_5_homework.py
@@ -0,0 +1,64 @@
+# 1 сложение чисел 7 + 15
+num_1 = 7
+num_2 = 15
+result_1 = num_1 + num_2
+
+print(result_1)
+
+# 2 вычитание чисел 25 - 10
+num_3 = 25
+num_4 = 10
+result_2 = num_3 - num_4
+
+print(result_2)
+
+# 3 умножение чисел 6 * 8
+num_5 = 6
+num_6 = 8
+result_3 = num_5 * num_6
+
+print(result_3)
+
+# 4 деление чисел 20 / 4
+num_7 = 20
+num_8 = 4
+result_4 = num_7 / num_8
+
+print(result_4)
+
+# 5 комбинация операций  (10 + 5) * 2
+num_10 = 5
+num_11 = 2
+result_5 = (num_4 + num_10) * num_11
+
+print(result_5)
+
+# 6 остаток от деления 17 % 3
+num_12 = 17
+num_13 = 3
+result_6 = num_12 % num_13
+
+print(result_6)
+
+# 7 несколько операций  (15 - 3) * (2 + 4) / 6
+result_7 = (num_2 - num_13) * (num_11 + num_8) / num_5
+
+print(result_7)
+
+# 8 дроби 22 // 7
+num_19 = 22
+result_8 = num_19 // num_1
+
+print(result_8)
+
+# 9 сложное выражение (10 + 20) / 5 + 3 * (8 - 2)
+result_9 = (num_4 + num_7) / num_10 + num_13 * (num_6 - num_11)
+
+print(result_9)
+
+# 10 операции со скобками 100 - (30 / (2 +3)) * 4 + 10
+num_21 = 100
+num_22 = 30
+result_10 = num_21 - (num_22 / (num_11 + num_13)) * num_8 + num_4
+
+print(result_10)
